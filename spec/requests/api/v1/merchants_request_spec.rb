955f1104828adfc80e7eaa5765f6f9407401ce61
Matt Levy <milevy1@gmail.com>
2019-04-30 11:07:37 -0600

    Added Merchant show

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -17,19 +17,41 @@ describe "Merchants API" do
                   "id" => "#{merchant_1.id}",
                     "type" => "merchant",
                     "attributes" => {
-                      "name" => "#{merchant_1.name}",
+                      "name" => "#{merchant_1.name}"
                     }
                 },
                 {
                   "id" => "#{merchant_2.id}",
                   "type" => "merchant",
                   "attributes" => {
-                    "name" => "#{merchant_2.name}",
+                    "name" => "#{merchant_2.name}"
                   }
                 }
                ]
               }
 
     expect(merchants).to eq(expected)
+  end
+
+  it 'sends a single merchant' do
+    merchant = create(:merchant)
+
+    get "/api/v1/merchants/#{merchant.id}.json"
+
+    expect(response).to be_successful
+
+    data = JSON.parse(response.body)
+
+    expected = {
+                "data" => {
+                  "id" => "#{merchant.id}",
+                    "type" => "merchant",
+                    "attributes" => {
+                      "name" => "#{merchant.name}"
+                    }
+                  }
+                }
+
+    expect(data).to eq(expected)
   end
 end
