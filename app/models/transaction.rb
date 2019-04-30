1bb4521e5ae0eaac63e1509179110da897c948e0
Matt Levy <milevy1@gmail.com>
2019-04-29 21:34:23 -0600

    Removed transacation validation for credit_card_expiration

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,7 +1,7 @@
 class Transaction < ApplicationRecord
   belongs_to :invoice
 
-  validates_presence_of :credit_card_expiration_date, :result
+  validates_presence_of :result
   validates :credit_card_number,
     presence: true,
     numericality: { only_integer: true }
