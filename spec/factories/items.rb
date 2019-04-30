7c24cadc28d7a8a2aa3da8ce777168992c526f64
Matt Levy <milevy1@gmail.com>
2019-04-29 19:32:04 -0600

    Revised factory for Item to use merchant association

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :item do
-    association :merchant, factory: :merchant
+    merchant
     sequence(:name) { |n| "Item Name #{n}" }
     sequence(:description) { |n| "Description #{n}" }
     sequence(:unit_price) { |n| ("#{n}".to_i + 1) * 2 }
