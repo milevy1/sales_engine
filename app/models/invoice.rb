285d2ddfb7e57ac0b20227bc49034e3280ff42b0
Matt Levy <milevy1@gmail.com>
2019-04-29 20:10:01 -0600

    Added Invoice relationships for invoice_items and items

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -1,6 +1,8 @@
 class Invoice < ApplicationRecord
   belongs_to :customer
   belongs_to :merchant
+  has_many :invoice_items
+  has_many :items, through: :invoice_items
 
   validates_presence_of :status
 end
--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -4,6 +4,8 @@ RSpec.describe Invoice, type: :model do
   describe 'relationships' do
     it { should belong_to :customer }
     it { should belong_to :merchant }
+    it { should have_many :invoice_items }
+    it { should have_many(:items).through(:invoice_items) }
   end
 
   describe 'validations' do
