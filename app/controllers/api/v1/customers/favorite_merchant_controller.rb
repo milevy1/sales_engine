5eecc981331a14337acc884a2ede9c6592279d40
Matt Levy <milevy1@gmail.com>
2019-05-02 16:07:36 -0600

    Refactored a deprecated SQL call

--- app/controllers/api/v1/customers/favorite_merchant_controller.rb
+++ app/controllers/api/v1/customers/favorite_merchant_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::Customers::FavoriteMerchantController < ApplicationController
   def show
     render json: MerchantSerializer.new(
-      Merchant.joins(invoices: [:transactions, :customer]).where("transactions.result = ? AND customers.id = ?", 'success', params[:id]).order("COUNT(transactions.result) DESC").group(:id).limit(1)[0]
+      Merchant.select("merchants.*, COUNT(transactions.result) as transaction_count").joins(invoices: [:transactions, :customer]).where("transactions.result = ? AND customers.id = ?", 'success', params[:id]).order("transaction_count DESC").group(:id).limit(1)[0]
     )
   end
 end
